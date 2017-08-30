e6f7b873885e8580a23eb4e3071b73e1f830484d
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-08-30 22:58:42 +0200

    there`s the traingle!

--- opengl/main.cc
+++ opengl/main.cc
@@ -21,10 +21,6 @@ public:
 			 0.0f,  0.5f, 0.0f
 		};
 
-		glGenBuffers(1, &vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-
 		// shaders
 		vertexShader = loadShader("shader.vert", GL_VERTEX_SHADER);
 		fragmentShader = loadShader("shader.frag", GL_FRAGMENT_SHADER);
@@ -34,12 +30,27 @@ public:
 		glDeleteShader(vertexShader); vertexShader = 0;
 		glDeleteShader(fragmentShader); fragmentShader = 0;
 
-		// shader params
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
 
 		// vertex array object
 		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		// vertex buffer object
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+
+		// shader params
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // uses currently bound VAO
+		glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+
+
+
+
 	}
 
 	void mainLoop()
@@ -79,21 +90,21 @@ private:
 
 	unsigned linkShaders(unsigned vertexShader, unsigned fragmentShader)
 	{
-		unsigned shaderProgram = glCreateProgram();
-		glAttachShader(shaderProgram, vertexShader);
-		glAttachShader(shaderProgram, fragmentShader);
-		glLinkProgram(shaderProgram);
+		unsigned sp = glCreateProgram();
+		glAttachShader(sp, vertexShader);
+		glAttachShader(sp, fragmentShader);
+		glLinkProgram(sp);
 
 		int  success;
 		char infoLog[512];
-		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		glGetProgramiv(sp, GL_LINK_STATUS, &success);
 		if(!success) {
-			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+			glGetProgramInfoLog(sp, 512, NULL, infoLog);
 			std::cout << "Error linking shaders" << std::endl;
 			std::cout << infoLog << std::endl;
 			throw std::runtime_error("Error linking shaders");
 		}
-		return shaderProgram;
+		return sp;
 	}
 
 	std::vector<float> vertices;
