b3f2c8f8f57d9b6a76868e684a333d84f9831649
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-08-30 22:46:08 +0200

    there`s no triangle :(

--- opengl/main.cc
+++ opengl/main.cc
@@ -28,12 +28,28 @@ public:
 		// shaders
 		vertexShader = loadShader("shader.vert", GL_VERTEX_SHADER);
 		fragmentShader = loadShader("shader.frag", GL_FRAGMENT_SHADER);
+		shaderProgram = linkShaders(vertexShader, fragmentShader);
+
+
+		glDeleteShader(vertexShader); vertexShader = 0;
+		glDeleteShader(fragmentShader); fragmentShader = 0;
+
+		// shader params
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		// vertex array object
+		glGenVertexArrays(1, &vao);
 	}
 
 	void mainLoop()
 	{
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 	}
 
 private:
@@ -61,10 +77,32 @@ private:
 		return shaderId;
 	}
 
+	unsigned linkShaders(unsigned vertexShader, unsigned fragmentShader)
+	{
+		unsigned shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glLinkProgram(shaderProgram);
+
+		int  success;
+		char infoLog[512];
+		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		if(!success) {
+			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+			std::cout << "Error linking shaders" << std::endl;
+			std::cout << infoLog << std::endl;
+			throw std::runtime_error("Error linking shaders");
+		}
+		return shaderProgram;
+	}
+
 	std::vector<float> vertices;
-	unsigned vbo = 0;
+	unsigned vbo = 0; // vertex buffer object
+	unsigned vao = 0; // vertex array object
+
 	unsigned vertexShader = 0;
 	unsigned fragmentShader = 0;
+	unsigned shaderProgram = 0;
 
 };
 
